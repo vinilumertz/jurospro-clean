b041d72d96dcc7cacef7db7ca841d8288b9770df
vinilumertz <vinielumertz@gmail.com>
2025-11-09 00:42:02 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div><h1>JurosPro</h1><p>App funcionando!</p></div>);
